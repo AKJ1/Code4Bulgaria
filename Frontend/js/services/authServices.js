04dc91897e6a49a4a8d65bb4f314a5cccd0fd2db
kotow <kotow@abv.bg>
2015-06-06 13:18:39 +0300

    ..

---
+++ Frontend/js/services/authServices.js
@@ -0,0 +1,67 @@
+app.factory('authService',
+    function ($http) {
+	var baseServiceUrl = '';
+        return {
+            login: function(userData) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/login',
+                    data: userData
+                };
+                 $http(request).success(function(data) {
+                 success(data);
+                }).error(error);
+            },
+
+            register: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/register',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    success(data);
+                }).error(error);
+            },
+
+            logout: function() {
+                delete sessionStorage['currentUser'];
+				var request = {
+			   method:'GET',
+			   usrl: baseServiceUrl + '/api/Account/Logout';
+			   };
+			   $http(request).success(function(data) {
+                    success(data);
+                }).error(error);
+            },
+
+            getCurrentUser : function() {
+               var request = {
+			   method:'GET',
+			   usrl: baseServiceUrl + '/api/Account/UserInfo';
+			   };
+			   $http(request);
+            },
+
+            getAuthHeaders : function() {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+                if (currentUser) {
+                    headers['Authorization'] = 'Bearer ' + currentUser.access_token;
+                }
+                return headers;
+            },
+			
+			externalLogin : function(){
+				 var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/Account/ExternalLogin',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    success(data);
+                }).error(error);
+			}
+        }
+    }
+);
---
+++ Frontend/js/services/signalData.js
@@ -0,0 +1,49 @@
+app.factory('signalService',
+    function ($http) {
+	var baseServiceUrl = '';
+        return {
+		createSignal:  function(data){
+			var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/Signal/Create',
+                    data: userData
+                };
+                 $http(request).success(function(data) {
+                 success(data);
+                }).error(error);
+			}
+		}
+		
+		resolveSignal:  function(){
+			var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/Signal/Resolve/{id}',
+                };
+                 $http(request).success(function(data) {
+                 success(data);
+                }).error(error);
+			}
+		}
+		
+		mySignals:  function(data){
+			var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/Signal/My',
+                };
+                 $http(request).success(function(data) {
+                 success(data);
+                }).error(error);
+			}
+		}
+		
+		getSignal:  function(id){
+			var request = {
+                    method: 'GET',
+                    url: baseServiceUrl + '/api/Signal/' + id,
+                };
+                 $http(request).success(function(data) {
+                 success(data);
+                }).error(error);
+			}
+		}
+	});
